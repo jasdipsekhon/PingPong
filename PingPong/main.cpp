7403e7c0adb0a1833896b19e0e2675f9a2fa787b
Jasdip Sekhon <jasdipsekhon@gmail.com>
2020-06-28 16:47:07 -0700

    Initial Commit

---
+++ PingPong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PingPong
+//
+//  Created by Jasdip Sekhon on 6/28/20.
+//  Copyright © 2020 Jasdip Sekhon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
