b926fa648be0eed16ac0fcd2e3d95cdd2eef0544
Jasdip Sekhon <jasdipsekhon@gmail.com>
2020-06-28 17:06:51 -0700

    Update main.cpp

--- PingPong/main.cpp
+++ PingPong/main.cpp
@@ -7,9 +7,9 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    
     return 0;
 }
